32c3d47242ea781d7a76f0b16500a29f488bff54
btclovebot <btclovebot@outlook.com>
2017-04-05 16:44:20 +0000

    some_message

---
+++ counterparty/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+__title__ = 'counterparty'
+__author__ = 'Oliver Morris <officialgamermorris@gmail.com> And Steven Douglas https://twitter.com/_myveryown '
+__repo__ = 'https://github.com/btclovebot/counterparty-api'
+
+from .core import Market
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+
+setup(
+   name = 'counterparty',
+   url = 'https://github.com/btclovebot/counterparty_api1',
+   download_url = 'https://github.com/btclovebot/counterparty_api1/archive/master.zip',
+   author = 'Oliver Morris And Steven Douglas',
+   description = 'Python API for counterpartychain.io',
+)
