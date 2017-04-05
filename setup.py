5ba5f14565538e53618fb5e0b1d9d8b3bd02de90
btclovebot <btclovebot@outlook.com>
2017-04-05 17:15:55 +0000

    some-message

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ from distutils.core import setup
 setup(
    name = 'counterparty',
    url = 'https://github.com/btclovebot/counterparty_api1',
-   download_url = 'https://github.com/btclovebot/counterparty_api1/archive/master.zip',
    author = 'Oliver Morris And Steven Douglas',
    description = 'Python API for counterpartychain.io',
+   packages=['counterparty'],
 )
